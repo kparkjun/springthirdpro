3134ba386fbdb8b436ce84b54ce0112089d78507
kparkjun <kparkjun4@gmail.com>
2023-08-05 16:42:49 +0900

    commit

---
+++ SpringThirdMVC06/src/main/java/kr/board/entity/Member.java
@@ -0,0 +1,20 @@
+package kr.board.entity;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private int memIdx;
+	private String memID;
+	private String memPassword;
+	private String memName;
+	private int memAge; //<- null,0
+	private String memGender;
+	private String memEmail;
+	private String memProfile;
+	private List<AuthVO> authList; 
+	//authList[0].auth,authList[1].auth,authList[2].auth
+	
+}
